38b34c21bbd7efb0e0beb0973fc982901439c05b
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2024-08-20 12:40:17 -0400

    Update plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.g.mapleader = " "
+local keymap = vim.keymap
+keymap.set("n", "<leader>jf", "<cmd>lua vim.lsp.buf.format({async = true})<CR>")
+keymap.set("v", "<leader>jf", "<cmd>lua vim.lsp.buf.format({async = true})<CR>")
